278261622f72622483f5d92c59241725587dfd2d
krrsalmane <karrounsalmane@gmail.com>
2025-09-22 17:48:17 +0100

    create the getters and setters

--- src/main/java/com/s/rattrouge/Entity/Book.java
+++ src/main/java/com/s/rattrouge/Entity/Book.java
@@ -1,11 +1,57 @@
 package com.s.rattrouge.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
+@Entity
 
-    public class Book {
-
+public class Book {
+@Id
+@GeneratedValue
             private int id;
             private String title;
             private String author;
             private boolean available;
+
+
+    public Book(int id, String title, String author, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = available;
+    }
+    public Book(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
